3c4a2b269e7521e54504c1880bee2095ff98f37a
John Wojewoda <wojewodajohn@gmail.com>
2023-04-27 13:44:43 -0400

    Added comment to .py file

--- jw_bikeshare.py
+++ jw_bikeshare.py
@@ -245,3 +245,5 @@ if __name__ == "__main__": # sometimes called the 'main guard' or 'main check'
 	main()
     
 # I need cite the work of ABDALLAH EL-SAWY whoes work helped me greatly. After attempting this myself, finding this web page online https://www.kaggle.com/code/abdallahmohamedamin/explore-us-bikeshare-data/notebook helped me understand this code. This, in conjunction with GhatGPT has made it so that I understand the conepts behind every line of the above code.
+
+##Adding comment for my git project
